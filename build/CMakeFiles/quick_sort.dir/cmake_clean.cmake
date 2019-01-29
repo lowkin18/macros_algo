8c4d7c28146f3a44d4108f64bb7cb63e38faa51d
lowkin18 <kip.kerluke@gmail.com>
2019-01-28 18:24:14 -0800

    cmake

---
+++ build/CMakeFiles/quick_sort.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quick_sort.dir/src/quick_sort.c.obj"
+  "CMakeFiles/quick_sort.dir/test/quick_sort_test.c.obj"
+  "CMakeFiles/quick_sort.dir/src/merge_sort.c.obj"
+  "quick_sort.pdb"
+  "quick_sort.exe"
+  "quick_sort.exe.manifest"
+  "libquick_sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quick_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
